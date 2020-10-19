48deb29167c9e6967e4bb9e69d2a2d6ba87e87e6
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-10-19 07:31:45 -0300

    fake writeasync to use async jobs

--- Controllers/JobsController.cs
+++ Controllers/JobsController.cs
@@ -2,21 +2,27 @@ using Hangfire;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Text.Encodings.Web;
+using System.Threading.Tasks;
 
 namespace aspnetjobs.Controllers
 {
-    public class JobsController : Controller
+  public class JobsController : Controller
+  {
+    public string Index()
     {
-        public string Index()
-        {
-            var numbers = Enumerable.Range(1, 100000).Select(x => x);
-            foreach (var num in numbers)
-            {
-                BackgroundJob.Enqueue(() => System.Console.WriteLine("Number is " + num));
-            }
+      var numbers = Enumerable.Range(1, 100000).Select(x => x);
+      foreach (var num in numbers)
+      {
+        BackgroundJob.Enqueue(() => WriteAsync(num));
+      }
 
-            return "This is my default action...";
-        }
-
+      return "This is my default action...";
     }
+
+    public async Task WriteAsync(int num)
+    {
+      System.Console.WriteLine("Number is " + num);
+      await Task.Delay(1);
+    }
+  }
 }
